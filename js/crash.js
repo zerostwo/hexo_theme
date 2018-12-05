545f12805f5cbf27fd5d0534954656d04fa0cb4f
zerostwo <zerostwo@163.com>
2018-12-05 12:22:23 +0800

    Site updated: 2018-12-05 12:22:22

--- js/crash.js
+++ js/crash.js
@@ -2,11 +2,11 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "favicon.png");
+        $('[rel="icon"]').attr('href', "/favicon.png");
         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
         clearTimeout(titleTime);
     } else {
-        $('[rel="icon"]').attr('href', "favicon.png");
+        $('[rel="icon"]').attr('href', "/favicon.png");
         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
