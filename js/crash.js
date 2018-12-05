e4e2ea42c41f88261872addd692ae1391eaa1a08
zerostwo <zerostwo@163.com>
2018-12-05 12:16:19 +0800

    Site updated: 2018-12-05 12:16:19

---
+++ js/crash.js
@@ -0,0 +1,15 @@
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "favicon.png");
+        document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+        clearTimeout(titleTime);
+    } else {
+        $('[rel="icon"]').attr('href', "favicon.png");
+        document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
